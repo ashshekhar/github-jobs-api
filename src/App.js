2e8355f9ea3ef246b8fed571d76ed393ce77f352
Shashwat Shekhar <svs6603@psu.edu>
2020-08-04 00:45:25 +0530

    Add search files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import FetchJobs from "./FetchJobs";
 import { Container } from "react-bootstrap";
 import Job from "./Job";
 import JobsPagination from "./JobsPagination";
+import SearchForm from "./SearchForm";
 
 function App() {
 	const [params, setParams] = useState({});
 	const [page, setPage] = useState(1);
 	const { jobs, loading, error, hasNextPage } = FetchJobs(params, page);
+
+	function handleParamChange(event) {
+		const params = event.target.name;
+		const value = event.target.value;
+		setPage(1);
+		setParams((prevParams) => {
+			return { ...prevParams, [params]: value };
+		});
+	}
 	return (
 		<Container className="my-4">
 			<h1 className="mb-4">GitHub Jobs</h1>
+			<SearchForm params={params} onParamChange={handleParamChange} />
 			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
 			{loading && <h1>Loading jobs...</h1>}
 			{error && <h1>Error loading jobs</h1>}
